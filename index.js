99a15c03f8434bca332227d7ee23cf7fb6e69cf7
Andrew <andrewasmit@hotmail.com>
2022-11-16 14:12:13 -0600

    added potential APIs to use.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+// const beerAPIBase = 'https://api.punkapi.com/v2/beers';
+// const tvShowAPIBase = 'http://api.tvmaze.com/search/shows?q=golden%20girls';
+const fdaRecallAPIBase = 'https://api.fda.gov/food/enforcement.json?limit=10';
+// There are plenty of attributes here to 
+
+const uniAPIBase = 'http://universities.hipolabs.com/search?country=United+Kingdom';
+// 3 Attributes can be name, webpage, and country
+
+
+
+fetch(fdaRecallAPIBase)
+.then(res=>res.json())
+.then(data=>console.log(data));
+
